102656d8c18c3a1f93ee1426d02d23badba8a721
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2023-02-28 14:20:43 +0330

    Login Panel Success

--- x-ui Selenium/Form1.Designer.cs
+++ x-ui Selenium/Form1.Designer.cs
@@ -35,8 +35,6 @@
             this.txtUrl = new System.Windows.Forms.TextBox();
             this.txtUsername = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.lblPath = new System.Windows.Forms.Label();
             this.txtPath = new System.Windows.Forms.TextBox();
             this.btnStart = new System.Windows.Forms.Button();
@@ -47,18 +45,16 @@
             // 
             this.bgInformation.Controls.Add(this.btnStart);
             this.bgInformation.Controls.Add(this.txtPath);
-            this.bgInformation.Controls.Add(this.textBox1);
             this.bgInformation.Controls.Add(this.txtPassword);
             this.bgInformation.Controls.Add(this.lblPath);
             this.bgInformation.Controls.Add(this.txtUsername);
-            this.bgInformation.Controls.Add(this.label1);
             this.bgInformation.Controls.Add(this.txtUrl);
             this.bgInformation.Controls.Add(this.lblPassword);
             this.bgInformation.Controls.Add(this.lblUsername);
             this.bgInformation.Controls.Add(this.lblUrl);
             this.bgInformation.Location = new System.Drawing.Point(13, 12);
             this.bgInformation.Name = "bgInformation";
-            this.bgInformation.Size = new System.Drawing.Size(336, 207);
+            this.bgInformation.Size = new System.Drawing.Size(336, 184);
             this.bgInformation.TabIndex = 0;
             this.bgInformation.TabStop = false;
             this.bgInformation.Text = "Panel Information : ";
@@ -96,6 +92,7 @@
             this.txtUrl.Name = "txtUrl";
             this.txtUrl.Size = new System.Drawing.Size(238, 22);
             this.txtUrl.TabIndex = 0;
+            this.txtUrl.Text = "https://ir2.hectortube.shop:2053/";
             // 
             // txtUsername
             // 
@@ -103,34 +100,21 @@
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(238, 22);
             this.txtUsername.TabIndex = 1;
+            this.txtUsername.Text = "admin";
             // 
             // txtPassword
             // 
             this.txtPassword.Location = new System.Drawing.Point(92, 84);
             this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '$';
             this.txtPassword.Size = new System.Drawing.Size(238, 22);
             this.txtPassword.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 115);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 14);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Password :";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(92, 112);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(238, 22);
-            this.textBox1.TabIndex = 3;
+            this.txtPassword.Text = "admin12";
             // 
             // lblPath
             // 
             this.lblPath.AutoSize = true;
-            this.lblPath.Location = new System.Drawing.Point(17, 148);
+            this.lblPath.Location = new System.Drawing.Point(17, 120);
             this.lblPath.Name = "lblPath";
             this.lblPath.Size = new System.Drawing.Size(40, 14);
             this.lblPath.TabIndex = 2;
@@ -138,31 +122,34 @@
             // 
             // txtPath
             // 
-            this.txtPath.Location = new System.Drawing.Point(92, 140);
+            this.txtPath.Location = new System.Drawing.Point(92, 112);
             this.txtPath.Name = "txtPath";
             this.txtPath.Size = new System.Drawing.Size(238, 22);
             this.txtPath.TabIndex = 4;
+            this.txtPath.Text = "C:\\Users\\moham\\OneDrive\\دسکتاپ";
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(92, 168);
+            this.btnStart.Location = new System.Drawing.Point(92, 140);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(238, 33);
             this.btnStart.TabIndex = 5;
             this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(359, 230);
+            this.ClientSize = new System.Drawing.Size(359, 203);
             this.Controls.Add(this.bgInformation);
             this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "X-UI Robot Selenium";
+            this.Load += new System.EventHandler(this.frmMain_Load);
             this.bgInformation.ResumeLayout(false);
             this.bgInformation.PerformLayout();
             this.ResumeLayout(false);
@@ -174,11 +161,9 @@
         private System.Windows.Forms.GroupBox bgInformation;
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.TextBox txtPath;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Label lblPath;
         private System.Windows.Forms.TextBox txtUsername;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtUrl;
         private System.Windows.Forms.Label lblPassword;
         private System.Windows.Forms.Label lblUsername;
--- x-ui Selenium/Form1.cs
+++ x-ui Selenium/Form1.cs
@@ -5,16 +5,79 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace x_ui_Selenium
 {
     public partial class frmMain : Form
     {
+        ChromeDriver chrome;
         public frmMain()
         {
             InitializeComponent();
         }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            chrome = new ChromeDriver(txtPath.Text);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            chrome.Navigate().GoToUrl(txtUrl.Text);
+
+            Sleep();
+
+            var usernameElement = chrome.FindElement(By.XPath("/html/body/section/main/div[2]/div/form/div[1]/div/div/span/span/input"));
+            usernameElement.SendKeys(txtUsername.Text);
+
+            Sleep();
+
+            var passwordElement = chrome.FindElement(By.XPath("/html/body/section/main/div[2]/div/form/div[2]/div/div/span/span/input"));
+            passwordElement.SendKeys(txtPassword.Text);
+
+            Sleep();
+
+            var loginButton = chrome.FindElement(By.XPath("/html/body/section/main/div[2]/div/form/div[3]/div/div/span/button"));
+            loginButton.Click();
+
+            Sleep();
+
+            var inboundslistButton = chrome.FindElement(By.XPath("/html/body/section/aside/div/ul/li[2]"));
+            inboundslistButton.Click();
+
+            Sleep();
+
+            var actionConfig = chrome.FindElement(By.XPath("/html/body/section/section/main/div/div/div[2]/div[2]/div/div/div/div/div/div/div/table/tbody/tr/td[2]/a"));
+            actionConfig.Click();
+
+            Sleep();
+
+            var actionEdit = chrome.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[1]"));
+            actionEdit.Click();
+
+            Sleep();
+
+            var addClient = chrome.FindElement(By.XPath("/html/body/div[4]/div/div[2]/div/div[2]/div[2]/form[2]/span/svg/path[2]"));
+            addClient.Click();
+
+            Sleep();
+        
+            var clientName = chrome.FindElement(By.XPath("/html/body/div[4]/div/div[2]"));
+            clientName.SendKeys("The Name Of God");
+
+            Sleep();
+        }
+
+
+        private static void Sleep(int seconds = 1000)
+        {
+            Thread.Sleep(seconds);
+        }
+
     }
 }
