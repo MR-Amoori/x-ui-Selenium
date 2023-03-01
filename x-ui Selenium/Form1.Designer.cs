1bc1129585882eb97f7a6ce6f78cfbe66d75a13e
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2023-03-01 15:00:01 +0330

    Fixed Bugs

--- x-ui Selenium/Form1.Designer.cs
+++ x-ui Selenium/Form1.Designer.cs
@@ -29,27 +29,28 @@
         private void InitializeComponent()
         {
             this.bgInformation = new System.Windows.Forms.GroupBox();
-            this.lblUrl = new System.Windows.Forms.Label();
-            this.lblUsername = new System.Windows.Forms.Label();
-            this.lblPassword = new System.Windows.Forms.Label();
-            this.txtUrl = new System.Windows.Forms.TextBox();
-            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtNumberConfig = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblUrl = new System.Windows.Forms.Label();
             this.btnLogin = new System.Windows.Forms.Button();
             this.gbNeeds = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.lbl = new System.Windows.Forms.Label();
-            this.txtFrom = new System.Windows.Forms.TextBox();
-            this.txtTo = new System.Windows.Forms.TextBox();
-            this.txtName = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.txtNumberConfig = new System.Windows.Forms.TextBox();
-            this.btnGetConfigs = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
             this.txtTotalTraffic = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.lbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGetConfigs = new System.Windows.Forms.Button();
             this.btnAddUser = new System.Windows.Forms.Button();
-            this.listConfigs = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
             this.bgInformation.SuspendLayout();
             this.gbNeeds.SuspendLayout();
             this.SuspendLayout();
@@ -59,18 +60,84 @@
             this.bgInformation.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.bgInformation.Controls.Add(this.txtNumberConfig);
             this.bgInformation.Controls.Add(this.txtPassword);
+            this.bgInformation.Controls.Add(this.txtPort);
             this.bgInformation.Controls.Add(this.txtUsername);
             this.bgInformation.Controls.Add(this.label4);
             this.bgInformation.Controls.Add(this.txtUrl);
+            this.bgInformation.Controls.Add(this.label3);
             this.bgInformation.Controls.Add(this.lblPassword);
             this.bgInformation.Controls.Add(this.lblUsername);
             this.bgInformation.Controls.Add(this.lblUrl);
             this.bgInformation.Location = new System.Drawing.Point(13, 12);
             this.bgInformation.Name = "bgInformation";
-            this.bgInformation.Size = new System.Drawing.Size(336, 143);
+            this.bgInformation.Size = new System.Drawing.Size(336, 178);
             this.bgInformation.TabIndex = 0;
             this.bgInformation.TabStop = false;
             this.bgInformation.Text = "Panel Information : ";
+            // 
+            // txtNumberConfig
+            // 
+            this.txtNumberConfig.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtNumberConfig.Location = new System.Drawing.Point(92, 140);
+            this.txtNumberConfig.Name = "txtNumberConfig";
+            this.txtNumberConfig.Size = new System.Drawing.Size(238, 22);
+            this.txtNumberConfig.TabIndex = 3;
+            this.txtNumberConfig.Text = "1";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtPassword.Location = new System.Drawing.Point(92, 112);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '$';
+            this.txtPassword.Size = new System.Drawing.Size(238, 22);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.Text = "admin12";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtUsername.Location = new System.Drawing.Point(92, 84);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(238, 22);
+            this.txtUsername.TabIndex = 1;
+            this.txtUsername.Text = "admin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 14);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Config :";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtUrl.Location = new System.Drawing.Point(92, 28);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(238, 22);
+            this.txtUrl.TabIndex = 0;
+            this.txtUrl.Text = "https://s1.mohsenvahedi.top";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(4, 115);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(66, 14);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password :";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(3, 87);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(69, 14);
+            this.lblUsername.TabIndex = 1;
+            this.lblUsername.Text = "Username :";
             // 
             // lblUrl
             // 
@@ -81,55 +148,9 @@
             this.lblUrl.TabIndex = 0;
             this.lblUrl.Text = "URL :";
             // 
-            // lblUsername
-            // 
-            this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(3, 59);
-            this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(69, 14);
-            this.lblUsername.TabIndex = 1;
-            this.lblUsername.Text = "Username :";
-            // 
-            // lblPassword
-            // 
-            this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(4, 87);
-            this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(66, 14);
-            this.lblPassword.TabIndex = 2;
-            this.lblPassword.Text = "Password :";
-            // 
-            // txtUrl
-            // 
-            this.txtUrl.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.txtUrl.Location = new System.Drawing.Point(92, 28);
-            this.txtUrl.Name = "txtUrl";
-            this.txtUrl.Size = new System.Drawing.Size(238, 22);
-            this.txtUrl.TabIndex = 0;
-            this.txtUrl.Text = "https://s1.mohsenvahedi.top:2053/";
-            // 
-            // txtUsername
-            // 
-            this.txtUsername.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.txtUsername.Location = new System.Drawing.Point(92, 56);
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(238, 22);
-            this.txtUsername.TabIndex = 1;
-            this.txtUsername.Text = "admin";
-            // 
-            // txtPassword
-            // 
-            this.txtPassword.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.txtPassword.Location = new System.Drawing.Point(92, 84);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.PasswordChar = '$';
-            this.txtPassword.Size = new System.Drawing.Size(238, 22);
-            this.txtPassword.TabIndex = 2;
-            this.txtPassword.Text = "admin12";
-            // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(10, 306);
+            this.btnLogin.Location = new System.Drawing.Point(12, 341);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(337, 33);
             this.btnLogin.TabIndex = 8;
@@ -148,101 +169,12 @@
             this.gbNeeds.Controls.Add(this.lbl);
             this.gbNeeds.Controls.Add(this.label2);
             this.gbNeeds.Controls.Add(this.label1);
-            this.gbNeeds.Location = new System.Drawing.Point(13, 161);
+            this.gbNeeds.Location = new System.Drawing.Point(13, 196);
             this.gbNeeds.Name = "gbNeeds";
             this.gbNeeds.Size = new System.Drawing.Size(334, 139);
             this.gbNeeds.TabIndex = 6;
             this.gbNeeds.TabStop = false;
             this.gbNeeds.Text = "Need : ";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 28);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 14);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "From :";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 56);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(30, 14);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "To :";
-            // 
-            // lbl
-            // 
-            this.lbl.AutoSize = true;
-            this.lbl.Location = new System.Drawing.Point(20, 84);
-            this.lbl.Name = "lbl";
-            this.lbl.Size = new System.Drawing.Size(46, 14);
-            this.lbl.TabIndex = 1;
-            this.lbl.Text = "Name :";
-            // 
-            // txtFrom
-            // 
-            this.txtFrom.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.txtFrom.Location = new System.Drawing.Point(92, 25);
-            this.txtFrom.Name = "txtFrom";
-            this.txtFrom.Size = new System.Drawing.Size(236, 22);
-            this.txtFrom.TabIndex = 4;
-            this.txtFrom.Text = "2";
-            // 
-            // txtTo
-            // 
-            this.txtTo.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.txtTo.Location = new System.Drawing.Point(92, 53);
-            this.txtTo.Name = "txtTo";
-            this.txtTo.Size = new System.Drawing.Size(236, 22);
-            this.txtTo.TabIndex = 5;
-            // 
-            // txtName
-            // 
-            this.txtName.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.txtName.Location = new System.Drawing.Point(92, 81);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(236, 22);
-            this.txtName.TabIndex = 6;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 115);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 14);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Config :";
-            // 
-            // txtNumberConfig
-            // 
-            this.txtNumberConfig.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.txtNumberConfig.Location = new System.Drawing.Point(92, 112);
-            this.txtNumberConfig.Name = "txtNumberConfig";
-            this.txtNumberConfig.Size = new System.Drawing.Size(238, 22);
-            this.txtNumberConfig.TabIndex = 3;
-            this.txtNumberConfig.Text = "1";
-            // 
-            // btnGetConfigs
-            // 
-            this.btnGetConfigs.Location = new System.Drawing.Point(10, 384);
-            this.btnGetConfigs.Name = "btnGetConfigs";
-            this.btnGetConfigs.Size = new System.Drawing.Size(337, 33);
-            this.btnGetConfigs.TabIndex = 9;
-            this.btnGetConfigs.Text = "Get Configs";
-            this.btnGetConfigs.UseVisualStyleBackColor = true;
-            this.btnGetConfigs.Click += new System.EventHandler(this.btnGetConfigs_Click);
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(3, 112);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(81, 14);
-            this.label5.TabIndex = 1;
-            this.label5.Text = "Total Traffic :";
             // 
             // txtTotalTraffic
             // 
@@ -253,9 +185,80 @@
             this.txtTotalTraffic.TabIndex = 7;
             this.txtTotalTraffic.Text = "30";
             // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtName.Location = new System.Drawing.Point(92, 81);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(236, 22);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtTo
+            // 
+            this.txtTo.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtTo.Location = new System.Drawing.Point(92, 53);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(236, 22);
+            this.txtTo.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 14);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Total Traffic :";
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtFrom.Location = new System.Drawing.Point(92, 25);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(236, 22);
+            this.txtFrom.TabIndex = 4;
+            this.txtFrom.Text = "2";
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(20, 84);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(46, 14);
+            this.lbl.TabIndex = 1;
+            this.lbl.Text = "Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "To :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From :";
+            // 
+            // btnGetConfigs
+            // 
+            this.btnGetConfigs.Location = new System.Drawing.Point(12, 419);
+            this.btnGetConfigs.Name = "btnGetConfigs";
+            this.btnGetConfigs.Size = new System.Drawing.Size(337, 33);
+            this.btnGetConfigs.TabIndex = 9;
+            this.btnGetConfigs.Text = "Get Configs";
+            this.btnGetConfigs.UseVisualStyleBackColor = true;
+            this.btnGetConfigs.Click += new System.EventHandler(this.btnGetConfigs_Click);
+            // 
             // btnAddUser
             // 
-            this.btnAddUser.Location = new System.Drawing.Point(10, 345);
+            this.btnAddUser.Location = new System.Drawing.Point(12, 380);
             this.btnAddUser.Name = "btnAddUser";
             this.btnAddUser.Size = new System.Drawing.Size(337, 33);
             this.btnAddUser.TabIndex = 9;
@@ -263,21 +266,29 @@
             this.btnAddUser.UseVisualStyleBackColor = true;
             this.btnAddUser.Click += new System.EventHandler(this.btnAddUser_Click);
             // 
-            // listConfigs
+            // label3
             // 
-            this.listConfigs.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.listConfigs.FormattingEnabled = true;
-            this.listConfigs.Location = new System.Drawing.Point(10, 423);
-            this.listConfigs.Name = "listConfigs";
-            this.listConfigs.Size = new System.Drawing.Size(337, 199);
-            this.listConfigs.TabIndex = 10;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 14);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Port :";
+            // 
+            // txtPort
+            // 
+            this.txtPort.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtPort.Location = new System.Drawing.Point(92, 56);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(238, 22);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "2053";
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(359, 632);
-            this.Controls.Add(this.listConfigs);
+            this.ClientSize = new System.Drawing.Size(359, 477);
             this.Controls.Add(this.gbNeeds);
             this.Controls.Add(this.btnAddUser);
             this.Controls.Add(this.btnGetConfigs);
@@ -320,7 +331,8 @@
         private System.Windows.Forms.TextBox txtTotalTraffic;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button btnAddUser;
-        private System.Windows.Forms.ListBox listConfigs;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- x-ui Selenium/Form1.cs
+++ x-ui Selenium/Form1.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.IO;
+using System.Linq.Expressions;
 
 namespace x_ui_Selenium
 {
@@ -29,63 +30,83 @@ namespace x_ui_Selenium
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            chrome.Navigate().GoToUrl(txtUrl.Text);
-            chrome.Manage().Window.Maximize();
+            Repeat:
 
-            Sleep(2000);
+            try
+            {
+                chrome.Navigate().GoToUrl(txtUrl.Text + ":" + txtPort.Text);
+                chrome.Manage().Window.Maximize();
 
-            var usernameElement = chrome.FindElement(By.XPath("/html/body/section/main/div[2]/div/form/div[1]/div/div/span/span/input"));
-            usernameElement.SendKeys(txtUsername.Text);
+                Sleep(2000);
+
+                var usernameElement = chrome.FindElement(By.XPath("/html/body/section/main/div[2]/div/form/div[1]/div/div/span/span/input"));
+                usernameElement.SendKeys(txtUsername.Text);
 
 
-            var passwordElement = chrome.FindElement(By.XPath("/html/body/section/main/div[2]/div/form/div[2]/div/div/span/span/input"));
-            passwordElement.SendKeys(txtPassword.Text);
+                var passwordElement = chrome.FindElement(By.XPath("/html/body/section/main/div[2]/div/form/div[2]/div/div/span/span/input"));
+                passwordElement.SendKeys(txtPassword.Text);
 
 
-            var loginButton = chrome.FindElement(By.XPath("/html/body/section/main/div[2]/div/form/div[3]/div/div/span/button"));
-            loginButton.Click();
+                var loginButton = chrome.FindElement(By.XPath("/html/body/section/main/div[2]/div/form/div[3]/div/div/span/button"));
+                loginButton.Click();
 
-            Sleep(5000);
+                Sleep(5000);
 
-            var inboundslistButton = chrome.FindElement(By.XPath("/html/body/section/aside/div/ul/li[2]"));
-            inboundslistButton.Click();
+                var inboundslistButton = chrome.FindElement(By.XPath("/html/body/section/aside/div/ul/li[2]"));
+                inboundslistButton.Click();
 
-            Sleep(5000);
+                Sleep();
+            }
+            catch 
+            {
+                goto Repeat;
+            }
 
         }
 
 
         private void btnAddUser_Click(object sender, EventArgs e)
         {
-            var actionConfig = chrome.FindElement(By.XPath($"/html/body/section/section/main/div/div/div[2]/div[2]/div/div/div/div/div/div/div/table/tbody/tr[{txtNumberConfig.Text}]/td[2]/a"));
-            actionConfig.Click();
+            Repeat:
 
-
-            var actionEdit = chrome.FindElement(By.XPath("//li[text()[normalize-space()='Edit']]"));
-            actionEdit.Click();
-
-            Sleep();
-
-            for (int i = Convert.ToInt32(txtFrom.Text); i <= Convert.ToInt32(txtTo.Text); i++)
+            try
             {
+                var actionConfig = chrome.FindElement(By.XPath($"/html/body/section/section/main/div/div/div[2]/div[2]/div/div/div/div/div/div/div/table/tbody/tr[{txtNumberConfig.Text}]/td[2]/a"));
+                actionConfig.Click();
 
-                var addClient = chrome.FindElement(By.XPath("//label[text()='Clients']/following-sibling::span"));
-                addClient.Click();
 
-                Sleep(250);
+                var actionEdit = chrome.FindElement(By.XPath("//li[text()[normalize-space()='Edit']]"));
+                actionEdit.Click();
 
-                var client = chrome.FindElement(By.XPath($"(//div[@class='ant-collapse-header'])[{i}]"));
-                client.Click();
+                Sleep();
 
-                Sleep(250);
-                                                                                        
-                var clientName = chrome.FindElement(By.CssSelector($"div#inbound-modal>div:nth-of-type(2)>div>div:nth-of-type(2)>div:nth-of-type(2)>form:nth-of-type(2)>div:nth-of-type({i})>div>div:nth-of-type(2)>div>form>div>div:nth-of-type(2)>div>span>input"));
-                clientName.Clear();
-                clientName.SendKeys($"{txtName.Text}-{i - 1}");
+                for (int i = Convert.ToInt32(txtFrom.Text); i <= Convert.ToInt32(txtTo.Text); i++)
+                {
 
-                var clientTotalTraffic = chrome.FindElement(By.XPath($"(//input[@class='ant-input-number-input'])[{i}]"));
-                clientTotalTraffic.SendKeys(txtTotalTraffic.Text);
+                    var addClient = chrome.FindElement(By.XPath("//label[text()='Clients']/following-sibling::span"));
+                    addClient.Click();
 
+                    Sleep(250);
+
+                    var client = chrome.FindElement(By.XPath($"(//div[@class='ant-collapse-header'])[{i}]"));
+                    client.Click();
+
+                    Sleep(250);
+
+                    var clientName = chrome.FindElement(By.CssSelector($"div#inbound-modal>div:nth-of-type(2)>div>div:nth-of-type(2)>div:nth-of-type(2)>form:nth-of-type(2)>div:nth-of-type({i})>div>div:nth-of-type(2)>div>form>div>div:nth-of-type(2)>div>span>input"));
+                    clientName.Clear();
+                    clientName.SendKeys($"{txtName.Text}-{i - 1}");
+
+                    var clientTotalTraffic = chrome.FindElement(By.XPath($"(//input[@class='ant-input-number-input'])[{i}]"));
+                    clientTotalTraffic.SendKeys(txtTotalTraffic.Text);
+
+                }
+            }
+            catch 
+            {
+                chrome.Navigate().Refresh();
+                Sleep();
+                goto Repeat;
             }
         }
 
@@ -96,25 +117,28 @@ namespace x_ui_Selenium
 
             try
             {
+                var plussElement = chrome.FindElement(By.XPath($"//td[@class='ant-table-row-expand-icon-cell']//div[1]"));
+                plussElement.Click();
+
                 for (int i = 0; i <= 999; i++)
                 {
-                    var plussElement = chrome.FindElement(By.XPath($"//td[@class='ant-table-row-expand-icon-cell']//div[1]"));
-                    plussElement.Click();
-
                     var qrElement = chrome.FindElement(By.XPath($"/html/body/section/section/main/div/div/div[2]/div[2]/div/div/div/div/div/div/div/table/tbody/tr[2]/td[2]/div/div/div/div/div/div/table/tbody/tr[{i + 1}]/td[5]"));
-                    configs.Add($"vless://{qrElement.Text}@s1.mohsenvahedi.top:2087?type=grpc&security=tls&serviceName=&sni=s1.mohsenvahedi.top&flow=#NameDN-");
-
-                    Sleep(3000);
+                    configs.Add($"vless://{qrElement.Text}@{txtUrl.Text}:{txtPort.Text}?type=grpc&security=tls&serviceName=&sni=dns.mohsenvahedi.top&flow=#NameDN-");
+                   
                 }
             }
             catch
             {
-                string path = Application.StartupPath + "Result.txt";
-
-                foreach (string config in configs)
-                {
-                    File.WriteAllText(path, config);
-                }
+                //string path =  "Result.txt";
+                //StringBuilder sbResult = new StringBuilder();
+                //int number = 1;
+                //foreach (string config in configs)
+                //{
+                //    sbResult.Append(number.ToString());
+                //    sbResult.AppendLine(config);
+                //    sbResult.AppendLine();  
+                //}
+                //File.WriteAllText(path, config);
             }
         }
 
