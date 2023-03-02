afbf035e0acf9cc0bc8c0a1421a2acc75a7babf2
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2023-03-02 10:18:31 +0330

    Add Telegram Bot

--- x-ui Selenium/Form1.Designer.cs
+++ x-ui Selenium/Form1.Designer.cs
@@ -47,7 +47,7 @@
             this.btnGetConfigs = new System.Windows.Forms.Button();
             this.btnLogin = new System.Windows.Forms.Button();
             this.bgInformation = new System.Windows.Forms.GroupBox();
-            this.txtNumberConfig = new System.Windows.Forms.TextBox();
+            this.txtSubDomain = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.txtPort = new System.Windows.Forms.TextBox();
             this.txtUsername = new System.Windows.Forms.TextBox();
@@ -57,11 +57,22 @@
             this.lblPassword = new System.Windows.Forms.Label();
             this.lblUsername = new System.Windows.Forms.Label();
             this.lblUrl = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.btnSendConfigs = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSubAddress = new System.Windows.Forms.TextBox();
+            this.btnStopBot = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtChatId = new System.Windows.Forms.TextBox();
             this.statusStrip1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.gbNeeds.SuspendLayout();
             this.bgInformation.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // statusStrip1
@@ -69,7 +80,7 @@
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1,
             this.pgBar});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 464);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 496);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(357, 22);
             this.statusStrip1.TabIndex = 11;
@@ -95,7 +106,7 @@
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(357, 464);
+            this.tabControl1.Size = new System.Drawing.Size(357, 496);
             this.tabControl1.TabIndex = 50;
             // 
             // tabPage1
@@ -108,17 +119,18 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 23);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(349, 437);
+            this.tabPage1.Size = new System.Drawing.Size(349, 469);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.groupBox1);
             this.tabPage2.Location = new System.Drawing.Point(4, 23);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(349, 437);
+            this.tabPage2.Size = new System.Drawing.Size(349, 469);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -134,9 +146,9 @@
             this.gbNeeds.Controls.Add(this.lbl);
             this.gbNeeds.Controls.Add(this.label2);
             this.gbNeeds.Controls.Add(this.label1);
-            this.gbNeeds.Location = new System.Drawing.Point(8, 182);
+            this.gbNeeds.Location = new System.Drawing.Point(8, 213);
             this.gbNeeds.Name = "gbNeeds";
-            this.gbNeeds.Size = new System.Drawing.Size(333, 139);
+            this.gbNeeds.Size = new System.Drawing.Size(333, 136);
             this.gbNeeds.TabIndex = 11;
             this.gbNeeds.TabStop = false;
             this.gbNeeds.Text = "Need : ";
@@ -213,37 +225,42 @@
             // 
             // btnAddUser
             // 
-            this.btnAddUser.Location = new System.Drawing.Point(8, 366);
+            this.btnAddUser.Location = new System.Drawing.Point(8, 394);
             this.btnAddUser.Name = "btnAddUser";
             this.btnAddUser.Size = new System.Drawing.Size(333, 33);
             this.btnAddUser.TabIndex = 10;
             this.btnAddUser.Text = "Add User";
             this.btnAddUser.UseVisualStyleBackColor = true;
+            this.btnAddUser.Click += new System.EventHandler(this.btnAddUser_Click);
             // 
             // btnGetConfigs
             // 
-            this.btnGetConfigs.Location = new System.Drawing.Point(8, 405);
+            this.btnGetConfigs.Location = new System.Drawing.Point(8, 433);
             this.btnGetConfigs.Name = "btnGetConfigs";
             this.btnGetConfigs.Size = new System.Drawing.Size(333, 29);
             this.btnGetConfigs.TabIndex = 11;
             this.btnGetConfigs.Text = "Get Configs";
             this.btnGetConfigs.UseVisualStyleBackColor = true;
+            this.btnGetConfigs.Click += new System.EventHandler(this.btnGetConfigs_Click);
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(8, 327);
+            this.btnLogin.Location = new System.Drawing.Point(8, 355);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(333, 33);
             this.btnLogin.TabIndex = 9;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click_1);
             // 
             // bgInformation
             // 
             this.bgInformation.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.bgInformation.Controls.Add(this.txtNumberConfig);
+            this.bgInformation.Controls.Add(this.txtSubAddress);
+            this.bgInformation.Controls.Add(this.txtSubDomain);
             this.bgInformation.Controls.Add(this.txtPassword);
             this.bgInformation.Controls.Add(this.txtPort);
+            this.bgInformation.Controls.Add(this.label7);
             this.bgInformation.Controls.Add(this.txtUsername);
             this.bgInformation.Controls.Add(this.label4);
             this.bgInformation.Controls.Add(this.txtUrl);
@@ -253,19 +270,19 @@
             this.bgInformation.Controls.Add(this.lblUrl);
             this.bgInformation.Location = new System.Drawing.Point(8, 6);
             this.bgInformation.Name = "bgInformation";
-            this.bgInformation.Size = new System.Drawing.Size(333, 170);
+            this.bgInformation.Size = new System.Drawing.Size(333, 201);
             this.bgInformation.TabIndex = 10;
             this.bgInformation.TabStop = false;
             this.bgInformation.Text = "Panel Information : ";
             // 
-            // txtNumberConfig
+            // txtSubDomain
             // 
-            this.txtNumberConfig.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.txtNumberConfig.Location = new System.Drawing.Point(92, 140);
-            this.txtNumberConfig.Name = "txtNumberConfig";
-            this.txtNumberConfig.Size = new System.Drawing.Size(235, 22);
-            this.txtNumberConfig.TabIndex = 4;
-            this.txtNumberConfig.Text = "1";
+            this.txtSubDomain.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtSubDomain.Location = new System.Drawing.Point(92, 140);
+            this.txtSubDomain.Name = "txtSubDomain";
+            this.txtSubDomain.Size = new System.Drawing.Size(235, 22);
+            this.txtSubDomain.TabIndex = 4;
+            this.txtSubDomain.Text = "s1";
             // 
             // txtPassword
             // 
@@ -298,11 +315,11 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 143);
+            this.label4.Location = new System.Drawing.Point(6, 143);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 14);
+            this.label4.Size = new System.Drawing.Size(80, 14);
             this.label4.TabIndex = 2;
-            this.label4.Text = "Config :";
+            this.label4.Text = "Sub Domain :";
             // 
             // txtUrl
             // 
@@ -311,12 +328,12 @@
             this.txtUrl.Name = "txtUrl";
             this.txtUrl.Size = new System.Drawing.Size(235, 22);
             this.txtUrl.TabIndex = 0;
-            this.txtUrl.Text = "https://s1.mohsenvahedi.top";
+            this.txtUrl.Text = "mohsenvahedi.top";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(18, 59);
+            this.label3.Location = new System.Drawing.Point(27, 59);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(38, 14);
             this.label3.TabIndex = 1;
@@ -325,7 +342,7 @@
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(4, 115);
+            this.lblPassword.Location = new System.Drawing.Point(13, 115);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(66, 14);
             this.lblPassword.TabIndex = 2;
@@ -334,7 +351,7 @@
             // lblUsername
             // 
             this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(3, 87);
+            this.lblUsername.Location = new System.Drawing.Point(12, 87);
             this.lblUsername.Name = "lblUsername";
             this.lblUsername.Size = new System.Drawing.Size(69, 14);
             this.lblUsername.TabIndex = 1;
@@ -345,15 +362,102 @@
             this.lblUrl.AutoSize = true;
             this.lblUrl.Location = new System.Drawing.Point(19, 28);
             this.lblUrl.Name = "lblUrl";
-            this.lblUrl.Size = new System.Drawing.Size(36, 14);
+            this.lblUrl.Size = new System.Drawing.Size(55, 14);
             this.lblUrl.TabIndex = 0;
-            this.lblUrl.Text = "URL :";
+            this.lblUrl.Text = "Domain :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.OldLace;
+            this.groupBox1.Controls.Add(this.btnStopBot);
+            this.groupBox1.Controls.Add(this.btnSendConfigs);
+            this.groupBox1.Controls.Add(this.txtChatId);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txtToken);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(343, 463);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bot Information : ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 14);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Token : ";
+            // 
+            // txtToken
+            // 
+            this.txtToken.Location = new System.Drawing.Point(66, 24);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.Size = new System.Drawing.Size(271, 22);
+            this.txtToken.TabIndex = 1;
+            // 
+            // btnSendConfigs
+            // 
+            this.btnSendConfigs.Location = new System.Drawing.Point(9, 91);
+            this.btnSendConfigs.Name = "btnSendConfigs";
+            this.btnSendConfigs.Size = new System.Drawing.Size(328, 23);
+            this.btnSendConfigs.TabIndex = 2;
+            this.btnSendConfigs.Text = "Start Bot";
+            this.btnSendConfigs.UseVisualStyleBackColor = true;
+            this.btnSendConfigs.Click += new System.EventHandler(this.btnSendConfigs_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(5, 171);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 14);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Sub Address :";
+            // 
+            // txtSubAddress
+            // 
+            this.txtSubAddress.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtSubAddress.Location = new System.Drawing.Point(92, 168);
+            this.txtSubAddress.Name = "txtSubAddress";
+            this.txtSubAddress.Size = new System.Drawing.Size(235, 22);
+            this.txtSubAddress.TabIndex = 4;
+            this.txtSubAddress.Text = "dns";
+            // 
+            // btnStopBot
+            // 
+            this.btnStopBot.Location = new System.Drawing.Point(10, 120);
+            this.btnStopBot.Name = "btnStopBot";
+            this.btnStopBot.Size = new System.Drawing.Size(328, 23);
+            this.btnStopBot.TabIndex = 2;
+            this.btnStopBot.Text = "Stop Bot";
+            this.btnStopBot.UseVisualStyleBackColor = true;
+            this.btnStopBot.Click += new System.EventHandler(this.btnStopBot_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(5, 55);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 14);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Chat ID :";
+            // 
+            // txtChatId
+            // 
+            this.txtChatId.Location = new System.Drawing.Point(66, 52);
+            this.txtChatId.Name = "txtChatId";
+            this.txtChatId.Size = new System.Drawing.Size(271, 22);
+            this.txtChatId.TabIndex = 1;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(357, 486);
+            this.ClientSize = new System.Drawing.Size(357, 518);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.statusStrip1);
             this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -361,15 +465,19 @@
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "X-UI Robot Selenium";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
             this.Load += new System.EventHandler(this.frmMain_Load);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
             this.gbNeeds.ResumeLayout(false);
             this.gbNeeds.PerformLayout();
             this.bgInformation.ResumeLayout(false);
             this.bgInformation.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -394,7 +502,7 @@
         private System.Windows.Forms.Button btnGetConfigs;
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.GroupBox bgInformation;
-        private System.Windows.Forms.TextBox txtNumberConfig;
+        private System.Windows.Forms.TextBox txtSubDomain;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.TextBox txtPort;
         private System.Windows.Forms.TextBox txtUsername;
@@ -405,6 +513,15 @@
         private System.Windows.Forms.Label lblUsername;
         private System.Windows.Forms.Label lblUrl;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSendConfigs;
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSubAddress;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnStopBot;
+        private System.Windows.Forms.TextBox txtChatId;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- x-ui Selenium/Form1.cs
+++ x-ui Selenium/Form1.cs
@@ -6,6 +6,8 @@ using System.Windows.Forms;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.IO;
+using Telegram.Bot.Types;
+using Telegram.Bot;
 
 namespace x_ui_Selenium
 {
@@ -24,16 +26,16 @@ namespace x_ui_Selenium
             tabControl1.TabPages[1].Text = "Telegram Bot";
         }
 
-        private void btnLogin_Click(object sender, EventArgs e)
+        private void btnLogin_Click_1(object sender, EventArgs e)
         {
         Repeat:
 
             try
             {
-                chrome.Navigate().GoToUrl(txtUrl.Text + ":" + txtPort.Text);
+                chrome.Navigate().GoToUrl("https://" + txtSubDomain.Text + "." + txtUrl.Text + ":" + txtPort.Text);
                 chrome.Manage().Window.Maximize();
 
-                Sleep();
+                Sleep(10000);
 
                 var usernameElement = chrome.FindElement(By.XPath("/html/body/section/main/div[2]/div/form/div[1]/div/div/span/span/input"));
                 usernameElement.SendKeys(txtUsername.Text);
@@ -46,7 +48,7 @@ namespace x_ui_Selenium
                 var loginButton = chrome.FindElement(By.XPath("/html/body/section/main/div[2]/div/form/div[3]/div/div/span/button"));
                 loginButton.Click();
 
-                Sleep();
+                Sleep(5000);
 
                 var inboundslistButton = chrome.FindElement(By.XPath("/html/body/section/aside/div/ul/li[2]"));
                 inboundslistButton.Click();
@@ -60,52 +62,44 @@ namespace x_ui_Selenium
 
         private void btnAddUser_Click(object sender, EventArgs e)
         {
-        Repeat:
 
-            try
+
+            var actionConfig = chrome.FindElement(By.XPath($"/html/body/section/section/main/div/div/div[2]/div[2]/div/div/div/div/div/div/div/table/tbody/tr[1]/td[2]/a"));
+            actionConfig.Click();
+
+
+            var actionEdit = chrome.FindElement(By.XPath("//li[text()[normalize-space()='Edit']]"));
+            actionEdit.Click();
+
+            Sleep();
+
+            pgBar.Maximum = int.Parse(txtTo.Text) - int.Parse(txtFrom.Text);
+            int number = 1;
+
+            for (int i = Convert.ToInt32(txtFrom.Text); i <= Convert.ToInt32(txtTo.Text); i++)
             {
-                var actionConfig = chrome.FindElement(By.XPath($"/html/body/section/section/main/div/div/div[2]/div[2]/div/div/div/div/div/div/div/table/tbody/tr[{txtNumberConfig.Text}]/td[2]/a"));
-                actionConfig.Click();
+                pgBar.Value = number;
 
+                var addClient = chrome.FindElement(By.XPath("//label[text()='Clients']/following-sibling::span"));
+                addClient.Click();
 
-                var actionEdit = chrome.FindElement(By.XPath("//li[text()[normalize-space()='Edit']]"));
-                actionEdit.Click();
+                Sleep(250);
 
-                Sleep();
+                var client = chrome.FindElement(By.XPath($"(//div[@class='ant-collapse-header'])[{i}]"));
+                client.Click();
 
-                pgBar.Maximum = int.Parse(txtTo.Text) - int.Parse(txtFrom.Text);
-                int number = 1;
+                Sleep(250);
 
-                for (int i = Convert.ToInt32(txtFrom.Text); i <= Convert.ToInt32(txtTo.Text); i++)
-                {
-                    pgBar.Value = number;
+                var clientName = chrome.FindElement(By.CssSelector($"div#inbound-modal>div:nth-of-type(2)>div>div:nth-of-type(2)>div:nth-of-type(2)>form:nth-of-type(2)>div:nth-of-type({i})>div>div:nth-of-type(2)>div>form>div>div:nth-of-type(2)>div>span>input"));
+                clientName.Clear();
+                clientName.SendKeys($"{txtName.Text}-{i - 1}");
 
-                    var addClient = chrome.FindElement(By.XPath("//label[text()='Clients']/following-sibling::span"));
-                    addClient.Click();
+                var clientTotalTraffic = chrome.FindElement(By.XPath($"(//input[@class='ant-input-number-input'])[{i}]"));
+                clientTotalTraffic.SendKeys(txtTotalTraffic.Text);
 
-                    Sleep(250);
-
-                    var client = chrome.FindElement(By.XPath($"(//div[@class='ant-collapse-header'])[{i}]"));
-                    client.Click();
-
-                    Sleep(250);
-
-                    var clientName = chrome.FindElement(By.CssSelector($"div#inbound-modal>div:nth-of-type(2)>div>div:nth-of-type(2)>div:nth-of-type(2)>form:nth-of-type(2)>div:nth-of-type({i})>div>div:nth-of-type(2)>div>form>div>div:nth-of-type(2)>div>span>input"));
-                    clientName.Clear();
-                    clientName.SendKeys($"{txtName.Text}-{i - 1}");
-
-                    var clientTotalTraffic = chrome.FindElement(By.XPath($"(//input[@class='ant-input-number-input'])[{i}]"));
-                    clientTotalTraffic.SendKeys(txtTotalTraffic.Text);
-
-                    number++;
-                }
+                number++;
             }
-            catch
-            {
-                chrome.Navigate().Refresh();
-                Sleep();
-                goto Repeat;
-            }
+
         }
 
         private void btnGetConfigs_Click(object sender, EventArgs e)
@@ -121,7 +115,7 @@ namespace x_ui_Selenium
                 {
                     pgBar.Value = number;
                     var qrElement = chrome.FindElement(By.XPath($"/html/body/section/section/main/div/div/div[2]/div[2]/div/div/div/div/div/div/div/table/tbody/tr[2]/td[2]/div/div/div/div/div/div/table/tbody/tr[{i + 1}]/td[5]"));
-                    configs.Add($"vless://{qrElement.Text}@{txtUrl.Text}:{txtPort.Text}?type=grpc&security=tls&serviceName=&sni=dns.mohsenvahedi.top&flow=#NameDN-");
+                    configs.Add($"vless://{qrElement.Text}@{txtSubAddress.Text + "." + txtUrl.Text}:{2087}?type=grpc&security=tls&serviceName=&sni={txtSubDomain.Text + "." + txtUrl.Text}&flow=#{txtName.Text}-{number}");
                     number++;
                 }
             }
@@ -138,7 +132,7 @@ namespace x_ui_Selenium
                     sbResult.AppendLine();
                     number++;
                 }
-                File.WriteAllText(path, sbResult.ToString());
+                System.IO.File.WriteAllText(path, sbResult.ToString());
             }
         }
 
@@ -147,5 +141,78 @@ namespace x_ui_Selenium
             Thread.Sleep(seconds);
         }
 
+
+        private static string Token = "";
+        private Thread botThread;
+        Telegram.Bot.TelegramBotClient bot;
+        private void btnSendConfigs_Click(object sender, EventArgs e)
+        {
+            Token = txtToken.Text;
+            botThread = new Thread(new ThreadStart(runBot));
+            botThread.Start();
+        }
+
+        void runBot()
+        {
+            bot = new Telegram.Bot.TelegramBotClient(Token);
+
+            int offset = 0;
+
+            while (true)
+            {
+                Update[] update = bot.GetUpdatesAsync(offset).Result;
+                foreach (var up in update)
+                {
+                    offset = up.Id + 1;
+
+                    if (up.Message == null)
+                        continue;
+                    var text = up.Message.Text.ToLower();
+                    var from = up.Message.From;
+                    var chatId = up.Message.Chat.Id;
+
+                    if (text.Contains("/start"))
+                    {
+                        List<string> configs = new List<string>();
+                        try
+                        {
+                            var plussElement = chrome.FindElement(By.XPath($"//td[@class='ant-table-row-expand-icon-cell']//div[1]"));
+                            plussElement.Click();
+
+                            for (int i = 0; i <= 999; i++)
+                            {
+                                var qrElement = chrome.FindElement(By.XPath($"/html/body/section/section/main/div/div/div[2]/div[2]/div/div/div/div/div/div/div/table/tbody/tr[2]/td[2]/div/div/div/div/div/div/table/tbody/tr[{i + 1}]/td[5]"));
+                                configs.Add($"vless://{qrElement.Text}@{txtSubAddress.Text + "." + txtUrl.Text}:{2087}?type=grpc&security=tls&serviceName=&sni={txtSubDomain.Text + "." + txtUrl.Text}&flow=#{txtName.Text}-{i + 1}");
+                            }
+                        }
+                        catch
+                        {
+                            int number = 1;
+                            foreach (string config in configs)
+                            {
+                                StringBuilder sbResult = new StringBuilder();
+                                sbResult.AppendLine($"Config {number}:");
+                                sbResult.AppendLine(config);
+                                bot.SendTextMessageAsync(chatId, sbResult.ToString());
+                                Sleep(2000);
+                                number++;
+                            }
+                        }
+
+                    }
+                }
+            }
+
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            botThread.Abort();
+        }
+
+        private void btnStopBot_Click(object sender, EventArgs e)
+        {
+            botThread.Abort();
+        }
     }
 }
